a575b04bf504f8051da1aebdf0853e70b8c6e1ce
juniper92 <heyjuniper92@gmail.com>
2021-08-04 12:37:37 +0900

    commit 0804_first

--- InsCloning/View/Profile/ProfileHeaderView.swift
+++ InsCloning/View/Profile/ProfileHeaderView.swift
@@ -16,20 +16,34 @@ struct ProfileHeaderView: View {
                     .scaledToFill()
                     .frame(width: 80, height: 80)
                     .clipShape(Circle())
+                    .padding(.leading)
                 
                 Spacer()
+                
+                HStack(spacing: 16) {
+                    UserStatView(value: 1, title: "Post")
+                    UserStatView(value: 2, title: "Followers")
+                    UserStatView(value: 3, title: "Following")
+                }.padding(.trailing, 32)
+                
             }
-        }
-    }
-}
-
-struct UserStatView {
-    var body: some View {
-        VStack {
-            Text("2")
-                .font(.system(size: 15, weight: .semibold))
             
-            Text("Followers")
+            Text("Guess Who?")
+                .font(.system(size: 15, weight: .semibold))
+                .padding([.leading, .top])
+            
+            Text("It's gonna be a profile message")
+                .font(.system(size: 15))
+                .padding(.leading)
+                .padding(.top, 1)
+            
+            HStack {
+                Spacer()
+
+                ProfileActionButtonView()
+                
+                Spacer()
+            }.padding(.top)
         }
     }
 }
