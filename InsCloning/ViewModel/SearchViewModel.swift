9c3c40c4b8697a1a384caece57e0eafa459531c4
juniper92 <heyjuniper92@gmail.com>
2021-08-12 17:01:44 +0900

    0812_17

--- InsCloning/ViewModel/SearchViewModel.swift
+++ InsCloning/ViewModel/SearchViewModel.swift
@@ -20,6 +20,14 @@ class SearchViewModel: ObservableObject {
         COLLECTION_USERS.getDocuments { snapshot, _ in
             guard let documents = snapshot?.documents else { return }
             
+            documents.forEach { snapshot in
+                guard let user = try? snapshot.data(as: User.self) else { return }
+                self.users.append(user)
+            }
+            
+            self.users = documents.compactMap({ try? $0.data(as: User.self) })
+            
+            print(self.users)
         }
     }
 }
