c524be2c46855e46df40351a56eed607e3ead3ed
juniper92 <heyjuniper92@gmail.com>
2021-08-12 16:45:24 +0900

    commit 0812_16

--- InsCloning/ViewModel/SearchViewModel.swift
+++ InsCloning/ViewModel/SearchViewModel.swift
@@ -10,5 +10,16 @@ import SwiftUI
 class SearchViewModel: ObservableObject {
     @Published var users = [User]()
     
+    init() {
+        fetchUsers()
+    }
     
+    /* Firestore : https://peterfriese.dev/swiftui-firebase-codable/ */
+    /* MVVM : https://docs.microsoft.com/ko-kr/archive/blogs/msgulfcommunity/understanding-the-basics-of-mvvm-design-pattern */
+    func fetchUsers() {
+        COLLECTION_USERS.getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            
+        }
+    }
 }
